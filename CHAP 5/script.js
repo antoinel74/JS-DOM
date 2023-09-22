1892349712af6da2f8d928cad00ca0696c9f54d6
antoinel74 <antoine.lansman@gmail.com>
2023-09-22 12:21:10 +0200

    finish 5.3

--- CHAP 5/script.js
+++ CHAP 5/script.js
@@ -4,6 +4,8 @@ const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + "s";
 };
 
+// Duplication + log function
+const logList = document.querySelector("ul");
 const clickOnSquare = (e) => {
   console.log(getElapsedTime());
   const colorClass = e.target.classList[1];
@@ -11,7 +13,6 @@ const clickOnSquare = (e) => {
   newSquare.classList.add("displayedsquare", colorClass);
   document.querySelector(".displayedsquare-wrapper").appendChild(newSquare);
   const newLog = document.createElement("li");
-  const logList = document.querySelector("ul");
   const log = document.createTextNode(
     `[${getElapsedTime()}] Created a new ${colorClass} square`
   );
@@ -21,7 +22,29 @@ const clickOnSquare = (e) => {
   logList.appendChild(newLog);
 };
 
+// Add event to duplicate on click
 const actionSquares = document.querySelectorAll(".actionsquare");
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener("click", clickOnSquare);
 }
+
+// Changing body bg when hitting spacebar
+let pageBody = document.querySelector("body");
+let rdmColor;
+let switchBg = () => {
+  rdmColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  pageBody.style.backgroundColor = rdmColor;
+};
+
+// event.which:32  === space bar key ID //
+window.addEventListener("keydown", function (event) {
+  if (event.which === 32) {
+    switchBg();
+    const newSpaceLog = document.createElement("li");
+    const spaceLog = document.createTextNode(
+      `[${getElapsedTime()}] Switching bg color to ${rdmColor}`
+    );
+    newSpaceLog.appendChild(spaceLog);
+    logList.appendChild(newSpaceLog);
+  }
+});
