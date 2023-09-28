3abb7ddc823e6c502ae98862e13899569cbb78b7
antoinel74 <antoine.lansman@gmail.com>
2023-09-28 11:57:51 +0200

    ex3 darkmode done

--- CHAP 6/script.js
+++ CHAP 6/script.js
@@ -13,7 +13,6 @@ let inputAge = document.getElementById("age");
 let displaySection = document.getElementById("a-hard-truth");
 
 inputAge.addEventListener("keyup", checkAge);
-console.log(inputAge);
 
 function checkAge() {
   let age = parseInt(inputAge.value);
@@ -21,5 +20,36 @@ function checkAge() {
     displaySection.style.visibility = "visible";
   } else {
     displaySection.style.visibility = "hidden";
+  }
+}
+
+// 3. Password verifier
+let inputPasswrd = document.getElementById("pwd");
+let secondPasswrd = document.getElementById("pwd-confirm");
+
+inputPasswrd.addEventListener("keyup", checkPasswrd);
+secondPasswrd.addEventListener("keyup", checkPasswrd);
+
+function checkPasswrd() {
+  if (inputPasswrd.value === secondPasswrd.value) {
+    secondPasswrd.style.backgroundColor = "#BCE29E";
+  } else {
+    secondPasswrd.style.backgroundColor = "#FF8787";
+  }
+}
+
+// 4. Dark mode toggle
+let mode = document.getElementById("toggle-darkmode");
+let body = document.querySelector("body");
+
+mode.addEventListener("change", switchMode);
+
+function switchMode() {
+  if (mode.value === "light") {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  } else {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
   }
 }
