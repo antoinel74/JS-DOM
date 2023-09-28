f0674501e4f0a46a0dce374574a6ddd38291485d
antoinel74 <antoine.lansman@gmail.com>
2023-09-28 10:23:01 +0200

    ex2 done

--- CHAP 6/script.js
+++ CHAP 6/script.js
@@ -1,3 +1,4 @@
+// 1. Display in real time the input value in the display-firstname span
 let inputName = document.getElementById("firstname");
 let displayName = document.getElementById("display-firstname");
 
@@ -5,4 +6,20 @@ inputName.addEventListener("keyup", updateName);
 
 function updateName(e) {
   displayName.textContent = e.target.value;
+}
+
+// 2. Display the section if age >= 18
+let inputAge = document.getElementById("age");
+let displaySection = document.getElementById("a-hard-truth");
+
+inputAge.addEventListener("keyup", checkAge);
+console.log(inputAge);
+
+function checkAge() {
+  let age = parseInt(inputAge.value);
+  if (age >= 18) {
+    displaySection.style.visibility = "visible";
+  } else {
+    displaySection.style.visibility = "hidden";
+  }
 }
