424c219312a0104a2d24475cf327f7279da303e9
antoinel74 <antoine.lansman@gmail.com>
2023-10-06 10:58:42 +0200

    optimizing

--- CHAP 10/script.js
+++ CHAP 10/script.js
@@ -1,11 +1,23 @@
 const loadBtn = document.getElementById("load-btn");
-const ulContainer = document.getElementById("ul-container");
 
 loadBtn.addEventListener("click", function () {
   fetch("ex.json")
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
+      const list = document.createElement("ul");
+      document.body.appendChild(list);
+      /*       json.forEach((data) => {
+        const items = document.createElement("li");
+        items.textContent = data;
+        list.appendChild(items);
+      }); */
+      for (let i = 0, length = json.length; i < length; i++) {
+        let data = json[i];
+        const items = document.createElement("li");
+        items.textContent = data;
+        list.appendChild(items);
+      }
     })
     .catch((error) => {
       console.log("Error detected!", error);
